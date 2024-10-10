38c80a9e54c3497bfb59886cac0f9580be704a93
Dylan Donnell <dylan.donnell@student.griffith.ie>
2024-10-10 21:54:47 +0100

    feat(games): add remaining hoyo games

---
+++ games/hsr/integration.lua
@@ -0,0 +1,33 @@
+return {
+    standard = 1,
+    editions = {
+        {
+            name = "global",
+            title = {
+                en = "Global",
+                ru = "Глобальная"
+            }
+        }
+    },
+    components = {},
+    game = {
+        get_status = function(edition: string)
+            return "installed"
+        end,
+
+        get_diff = function(edition: string)
+            return nil
+        end,
+
+        get_launch_info = function(edition: string)
+            return {
+                status = "disabled",
+                hint = {
+                    en = "It's a test implementation not meant for real use",
+                    ru = "Это тестовая реализация, не предназначенная для реального использования"
+                },
+                binary = ""
+            }
+        end
+    }
+}
